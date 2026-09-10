c086775cb64c7d6ada63530c0cb50ae3ccfce841
Manoj Gaikwad <manoj@Manojs-MacBook-Pro.local>
2026-09-10 19:03:55 +0530

    projects/ui-kit/src/lib/directives/tooltip.ts updated

--- projects/ui-kit/src/lib/directives/tooltip.ts
+++ projects/ui-kit/src/lib/directives/tooltip.ts
@@ -1,8 +1,22 @@
-import { Directive } from '@angular/core';
+import { Directive, ElementRef, HostListener, inject, input } from '@angular/core';
 
 @Directive({
   selector: '[agileTooltip]',
+  standalone: true
 })
-export class Tooltip {
-  constructor() {}
+export class TooltipDirective {
+  // v16 Input Signal API
+  appTooltip = input.required<string>();
+
+  private readonly el = inject(ElementRef<HTMLElement>);
+
+  @HostListener('mouseenter')
+  onMouseEnter(): void {
+    this.el.nativeElement.title = this.appTooltip();
+  }
+
+  @HostListener('mouseleave')
+  onMouseLeave(): void {
+    this.el.nativeElement.title = '';
+  }
 }
