339dea77a11b02d688d2a84548ece79217b6adcf
Manoj Gaikwad <manoj@Manojs-MacBook-Pro.local>
2026-09-10 19:03:07 +0530

    projects/ui-kit/src/lib/directives/draggable.ts updated

--- projects/ui-kit/src/lib/directives/draggable.ts
+++ projects/ui-kit/src/lib/directives/draggable.ts
@@ -1,8 +1,29 @@
-import { Directive } from '@angular/core';
+import { Directive, HostBinding, HostListener, signal } from '@angular/core';
 
 @Directive({
   selector: '[agileDraggable]',
+  standalone: true
 })
-export class Draggable {
-  constructor() {}
+export class DraggableDirective {
+  readonly isDragging = signal<boolean>(false);
+
+  @HostBinding('attr.draggable') readonly draggable = true;
+
+  @HostBinding('class.opacity-50')
+  get opacityClass(): boolean {
+    return this.isDragging();
+  }
+
+  @HostListener('dragstart', ['$event'])
+  onDragStart(event: DragEvent): void {
+    this.isDragging.set(true);
+    if (event.dataTransfer) {
+      event.dataTransfer.effectAllowed = 'move';
+    }
+  }
+
+  @HostListener('dragend')
+  onDragEnd(): void {
+    this.isDragging.set(false);
+  }
 }
